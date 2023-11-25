8175eb01cc124229148c76168509559d70ab3ed2
dhinesh-star <dhineshmanickam2001@gmail.com>
2023-11-26 00:02:17 +0530

    Code changes done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+        RWOnly rwOnly = new RWOnly();
+        rwOnly.setName("Dhinesh");
+        System.out.println(rwOnly.getName());
+    }
   
 }
